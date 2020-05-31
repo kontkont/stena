36ac00ad0d6ce82cc7f236a72add32fe5559d199
kontkont <kontanistovigor@yandex.ru>
2020-06-01 02:46:41 +0300

    Добавил востановление пароля по почте.

---
+++ authorization_reset_password.php
@@ -0,0 +1,35 @@
+<details>
+    <summary>Востановление пароля</summary>
+
+    <form action="" method="post">
+        <input type="text" placeholder="Ваш логин" name="loginResetPassword" required>
+        <input type="submit" value="Востановить пароль">
+    </form>
+</details>
+
+<?php
+
+include_once 'connection_stena.php';
+
+include_once "function.php";
+
+$loginResetPassword = $_POST['loginResetPassword'];
+$checkResetLogin = loginСheck($connection, $_POST, 'loginResetPassword', 'users_stena', 'login');
+
+if ($loginResetPassword && $checkResetLogin == TRUE)
+{
+    $resetPasswordMail = showKeyValue($connection, 'email','users_stena','login', $loginResetPassword);
+    $oldPasswordReset = showKeyValue($connection, 'password','users_stena','login',$loginResetPassword);
+
+    echo 'Пароль отправлен на ваш email. ';
+    mail($resetPasswordMail,
+        'Востановление пароля.',
+        "Ваш пароль - '$oldPasswordReset'. ");
+
+}
+elseif ($loginResetPassword && $checkResetLogin == FALSE)
+{
+    echo 'Пользователя с таким логином не существует. ';
+}
+
+?>
