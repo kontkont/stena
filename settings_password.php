021be936fed40cffb22b1c0fa9363044a216d2a5
kontkont <kontanistovigor@yandex.ru>
2020-06-01 02:45:59 +0300

    Добавил изменение пароля в личном кабинете.

---
+++ settings_password.php
@@ -0,0 +1,41 @@
+<details>
+    <summary>Изменить пароль</summary>
+
+    <form action="" method="post">
+        <input type="text" placeholder="Старый пароль" name="oldChangePassword" required>
+        <input type="text" placeholder="Новый пароль" name="changePasswordOne" required>
+        <input type="text" placeholder="Новый пароль еще раз" name="changePasswordTwo" required>
+        <input type="submit" value="Изменить пароль">
+    </form>
+</details>
+
+<?php
+
+include_once 'connection_stena.php';
+
+include_once "function.php";
+
+if ($_POST['oldChangePassword'] && $_SESSION['password'] == $_POST['oldChangePassword'])
+{
+    if ($_POST['changePasswordOne'] == $_POST['changePasswordTwo'])
+    {
+        $newChangePassword = $_POST['changePasswordOne'];
+        $oldChangeLogin = $_SESSION['login'];
+        $connection->query("UPDATE users_stena 
+                                     SET password ='$newChangePassword' 
+                                     WHERE login = '$oldChangeLogin';");
+
+        session_destroy();
+        header('Location: index.php');
+    }
+    elseif ($_POST['changePasswordOne'] !== $_POST['changePasswordTwo'])
+    {
+        echo 'Указанные НОВЫЕ пароли не совпадают. ';
+    }
+}
+elseif ($_POST['oldChangePassword'] && $_SESSION['password'] !== $_POST['oldChangePassword'])
+{
+    echo 'Cтарый пароль неверен. ';
+}
+
+?>
