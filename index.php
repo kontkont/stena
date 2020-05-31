55fe8063a2aa544f2805779d554e420facf19d46
kontkont <kontanistovigor@yandex.ru>
2020-06-01 02:43:05 +0300

    Добавил востановление по почте, изменение почты, изменение пароля.

--- index.php
+++ index.php
@@ -7,10 +7,13 @@ if (!$_SESSION['login'] && !$_SESSION['password'])
 {
     include 'registration.php';
     include 'authorization.php';
+    include 'authorization_reset_password.php';
 }
 elseif ($_SESSION['login'] && $_SESSION['password'])
 {
     include 'profile.php';
+    include 'settings_mail.php';
+    include 'settings_password.php';
     include 'comments.php';
 }
 
